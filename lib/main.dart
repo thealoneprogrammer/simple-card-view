24e633d018157fb88dfd7e677a7687352d0445a2
Sujiiii <sujithsuji1098@gmail.com>
2019-10-01 10:07:12 +0530

    added card view

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(color: Colors.green),
+        primarySwatch: Colors.purple,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Simple Card view'),
+      ),
+      body: new ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (context, i) {
+          return Container(
+            height: 130,
+            child: Card(
+//                color: Colors.blue,
+              elevation: 10,
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: GestureDetector(
+                      onTap: () {
+
+                      },
+                      child: Container(
+                        width: 100.0,
+                        height: 100.0,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            image: DecorationImage(
+                                image: AssetImage('images/hacker.jpeg'),
+                                fit: BoxFit.cover),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(75.0)),
+                            boxShadow: [
+                              BoxShadow(blurRadius: 7.0, color: Colors.black)
+                            ]),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      return showDialog<void>(
+                        context: context,
+                          barrierDismissible: false,
+                          builder: (BuildContext conext) {
+                          return AlertDialog(
+                            title: Text('Not in stock'),
+                            content:
+                                const Text('This item is no longer available'),
+                            actions: <Widget>[
+                              FlatButton(
+                                child: Text('Ok'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: Container(
+                        padding: EdgeInsets.all(30.0),
+                        child: Chip(
+                          label: Text('@anonymous'),
+                          shadowColor: Colors.blue,
+                          backgroundColor: Colors.green,
+                          elevation: 10,
+                          autofocus: true,
+                        )),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
